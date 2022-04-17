4cbb73856f728be8bf3304dec808d0f9c9482883
horioka <1735076@stu.kobe-u.ac.jp>
2022-04-18 03:33:00 +0900

    fix

--- W02/vec3.js
+++ W02/vec3.js
@@ -45,22 +45,16 @@ class Vec3 {
         this.x = y * v.z - z * v.y;
         this.y = z * v.x - x * v.z;
         this.z = x * v.y - y * v.x;
-        console.log("this=" + this)
         return this;
     }
 
     length() {
-        console.log("this.x=" + this.x)
-
         return Math.sqrt( this.x * this.x + this.y * this.y + this.z * this.z );
     }
 
     areaOfTriangle(v1, v2) {
         var a1 = v1.sub(this)
         var a2 = v2.sub(this)
-        console.log("a1=" + a1)
-        console.log("a2=" + a2)
-        console.log("ans=" + a1.cross(a2).length())
         return a1.cross(a2).length()
     }
 }
