b7d52fcf13a95c0181042b280fb500b110f4482d
horioka <1735076@stu.kobe-u.ac.jp>
2022-05-10 12:45:27 +0900

    fix

--- W06/w06_task02_main.js
+++ W06/w06_task02_main.js
@@ -1,30 +1,29 @@
 d3.csv("https://mh248.github.io/InfoVis2022/W04/data.csv")
-    .then( data => {
-        data.forEach( d => { d.x = +d.x; d.y = +d.y; });
+    .then(data => {
+        data.forEach(d => { d.x = +d.x; d.y = +d.y; });
 
         var config = {
             parent: '#drawing_region',
             width: 256,
             height: 256,
-            margin: {top:10, right:10, bottom:20, left:10}
+            margin: { top: 10, right: 10, bottom: 20, left: 10 }
         };
 
-        const scatter_plot = new ScatterPlot( config, data );
+        const scatter_plot = new ScatterPlot(config, data);
         scatter_plot.update();
     })
-    .catch( error => {
-        console.log( error );
+    .catch(error => {
+        console.log(error);
     });
 
 class ScatterPlot {
 
-    constructor( config, data ) {
+    constructor(config, data) {
         this.config = {
             parent: config.parent,
             width: config.width || 256,
             height: config.height || 256,
-            margin: config.margin || {top:10, right:10, bottom:10, left:10},
-            alpha: config.alpha || 20
+            margin: config.margin || { top: 10, right: 10, bottom: 10, left: 10 },
         }
         this.data = data;
         this.init();
@@ -33,46 +32,45 @@ class ScatterPlot {
     init() {
         let self = this;
 
-        self.svg = d3.select( self.config.parent )
+        self.svg = d3.select(self.config.parent)
             .attr('width', self.config.width)
             .attr('height', self.config.height);
 
         self.chart = self.svg.append('g')
-            .attr('transform', `translate(${self.config.margin.left}, ${self.config.margin.top})`);
+            .attr('transform', `translate(${self.config.margin.left*2}, ${self.config.margin.top})`);
 
         self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
         self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
 
         self.xscale = d3.scaleLinear()
-            .range( [0, self.inner_width - self.margin.left - self.margin.right] );
+            .range([0, self.inner_width+self.config.margin.left]);
 
         self.yscale = d3.scaleLinear()
-            .range( [0, self.inner_height - self.margin.top - self.margin.bottom] );
+            .range([0, self.inner_height+self.config.margin.top]);
 
-        self.xaxis = d3.axisBottom( self.xscale )
-            .ticks(6);
+        self.xaxis = d3.axisBottom(self.xscale)
+            .ticks(8);
 
         self.xaxis_group = self.chart.append('g')
-            .attr('transform', `translate(${self.config.margin.left}, ${self.inner_height + self.config.margin.top})`);
+            .attr('transform', `translate(0, ${self.inner_height})`);
 
-        self.yaxis = d3.axisLeft( self.yscale )
-            .ticks(6);
+        self.yaxis = d3.axisLeft(self.yscale)
+            .ticks(8);
 
         self.yaxis_group = self.chart.append('g')
-            .attr('transform', `translate(${self.config.margin.left}, ${self.config.margin.top})`);
+            .attr('transform', `translate(0, 0)`);
     }
 
     update() {
         let self = this;
 
-        const xmin = d3.min( self.data, d => d.x );
-        const xmax = d3.max( self.data, d => d.x );
-        self.xscale.domain( [xmin-10, xmax+10] );
+        const xmin = d3.min(self.data, d => d.x);
+        const xmax = d3.max(self.data, d => d.x);
+        self.xscale.domain([xmin - 20, xmax + 20]);
 
-        const ymin = d3.min( self.data, d => d.y );
-        const ymax = d3.max( self.data, d => d.y );
-        self.yscale.domain( [ymin-10, ymax+10] );
-
+        const ymin = d3.min(self.data, d => d.y);
+        const ymax = d3.max(self.data, d => d.y);
+        self.yscale.domain([ymax + 20, ymin - 20]);
         self.render();
     }
 
@@ -83,15 +81,15 @@ class ScatterPlot {
             .data(self.data)
             .enter()
             .append("circle")
-            .attr("cx", d => self.xscale( d.x ) )
-            .attr("cy", d => self.yscale( d.y ) )
-            .attr("r", d => d.r );
+            .attr("cx", d => self.xscale(d.x))
+            .attr("cy", d => self.yscale(d.y))
+            .attr("r", d => d.r);
 
         self.xaxis_group
-            .call( self.xaxis );
-        
+            .call(self.xaxis);
+
         self.yaxis_group
-            .call( self.yaxis );
+            .call(self.yaxis);
 
     }
 }
