36e2c0a5c8727409f09485b62bb8cccca0fefc6b
horioka <1735076@stu.kobe-u.ac.jp>
2022-05-10 13:08:45 +0900

    fix

--- W06/w06_task02_main.js
+++ W06/w06_task02_main.js
@@ -6,7 +6,7 @@ d3.csv("https://mh248.github.io/InfoVis2022/W04/data.csv")
             parent: '#drawing_region',
             width: 300,
             height: 300,
-            margin: { top: 10, right: 20, bottom: 35, left: 20 }
+            margin: { top: 10, right: 20, bottom: 35, left: 25 }
         };
 
         const scatter_plot = new ScatterPlot(config, data);
