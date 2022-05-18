b285b552499688e9185414d700b1d79f04f3e3e4
horioka <1735076@stu.kobe-u.ac.jp>
2022-05-18 14:09:14 +0900

    fix

--- W10/w10_task1_main.js
+++ W10/w10_task1_main.js
@@ -6,7 +6,7 @@ d3.csv("https://mh248.github.io/InfoVis2022/W08/w08_task1_data.csv")
             parent: '#drawing_region',
             width: 256,
             height: 128,
-            margin: {top:10, right:10, bottom:20, left:60},
+            margin: { top: 10, right: 10, bottom: 20, left: 60 },
         };
 
         const bar_chart = new BarChart(config, data);
@@ -21,16 +21,17 @@ class BarChart {
             parent: config.parent,
             width: config.width || 256,
             height: config.height || 128,
-            margin: config.margin || {top:10, right:10, bottom:20, left:60},
+            margin: config.margin || { top: 10, right: 10, bottom: 20, left: 60 },
         }
         this.data = data;
         this.init();
-
+        console.log(this.data)
         d3.select('#reverse')
             .on('click', d => {
-            this.data.reverse();
-            this.update();
-    });
+                this.data.reverse();
+                console.log(this.data)
+                this.update();
+            });
     }
     init() {
         let self = this;
@@ -38,33 +39,32 @@ class BarChart {
         self.svg = d3.select(self.config.parent)
             .attr('width', self.config.width)
             .attr('height', self.config.height);
-        
+
         self.chart = self.svg.append('g')
             .attr('transform', `translate(${self.config.margin.left}, ${self.config.margin.top})`);
-        console.log(self.chart)
         self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
         self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
-        
+
         self.xscale = d3.scaleLinear()
             .range([0, self.inner_width]);
-        
+
         self.yscale = d3.scaleBand()
             .range([0, self.inner_height])
             .paddingInner(0.1);
 
-        self.xaxis = d3.axisBottom( self.xscale )
+        self.xaxis = d3.axisBottom(self.xscale)
             .ticks(5)
             .tickSizeOuter(0);
 
         self.xaxis_group = self.chart.append('g')
             .attr('transform', `translate(0, ${self.inner_height})`)
-            .call( self.xaxis );
+            .call(self.xaxis);
 
-        self.yaxis = d3.axisLeft( self.yscale )
+        self.yaxis = d3.axisLeft(self.yscale)
             .tickSizeOuter(0);
 
         self.yaxis_group = self.chart.append('g')
-            .call( self.yaxis );
+            .call(self.yaxis);
     }
     update() {
         let self = this;
@@ -87,10 +87,10 @@ class BarChart {
             .attr("y", d => self.yscale(d.label))
             .attr("width", d => self.xscale(d.value))
             .attr("height", self.yscale.bandwidth());
-        
+
         self.xaxis_group
             .call(self.xaxis);
-        
+
         self.yaxis_group
             .call(self.yaxis);
     }
