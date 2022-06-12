064aaee07ac3f70b37e2534f19c208eb95f53550
horioka <1735076@stu.kobe-u.ac.jp>
2022-06-12 22:10:12 +0900

    fix

--- Task/task_RadarChart.js
+++ Task/task_RadarChart.js
@@ -1,10 +1,10 @@
 class RadarChart {
-    constructor (config, data) {
+    constructor(config, data) {
         this.config = {
             parent: config.parent,
             width: config.width || 256,
             height: config.height || 256,
-            margin: config.margin || {top:10, right:10, bottom:10, left:10},
+            margin: config.margin || { top: 10, right: 10, bottom: 10, left: 10 },
             xlabel: config.xlabel || '',
             ylabel: config.ylabel || '',
             cscale: config.cscale
@@ -23,27 +23,27 @@ class RadarChart {
         self.chart = self.svg.append('g')
             .attr('transform', `translate(${self.config.margin.left}, ${self.config.margin.top})`);
 
-        self.grid = [[1,1,1],[2,2,2],[3,3,3],[4,4,4],[5,5,5]]
-        self.label = ['合計','生活','事務']
+        self.grid = [[1, 1, 1], [2, 2, 2], [3, 3, 3], [4, 4, 4], [5, 5, 5]]
+        self.label = ['合計', '生活', '事務']
     }
 
     update() {
         let self = this;
 
-        const data_map = d3.rollup( self.data, v => v.length, d => d.prefecture );
+        const data_map = d3.rollup(self.data, v => v.length, d => d.prefecture);
         console.log(data_map)
         const line_data_kochi = self.data.filter(d => d.prefecture == '高知')
-        line_data_kochi.reduce(function(s, element){return s+element.sum},0)/line_data_kochi.length
-        self.aggregated_data = Array.from( data_map, ([key,count]) => ({key,count}) );
+        line_data_kochi.reduce(function (s, element) { return s + element.sum }, 0) / line_data_kochi.length
+        self.aggregated_data = Array.from(data_map, ([key, count]) => ({ key, count }));
 
-        self.new_data = [[3,4,6],[7,4,2]]
+        self.new_data = [[3, 4, 6], [7, 4, 2]]
 
         self.rScale = d3.scaleLinear()
             .domain([0, 10])
-            .range([0, self.config.width/2 - self.config.margin.top])
+            .range([0, self.config.width / 2 - self.config.margin.top])
         self.line = d3.line()
-            .x(function(d,i){return self.rScale(d) * Math.cos(2 * Math.PI / 3 * i - (Math.PI / 2)) + self.config.width/2;})
-            .y(function(d,i){return self.rScale(d) * Math.sin(2 * Math.PI / 3 * i - (Math.PI / 2)) + self.config.width/2;})
+            .x(function (d, i) { return self.rScale(d) * Math.cos(2 * Math.PI / 3 * i - (Math.PI / 2)) + self.config.width / 2; })
+            .y(function (d, i) { return self.rScale(d) * Math.sin(2 * Math.PI / 3 * i - (Math.PI / 2)) + self.config.width / 2; })
         self.render();
     }
 
@@ -53,15 +53,15 @@ class RadarChart {
             .data(self.new_data)
             .enter()
             .append("path")
-            .attr("d", function(d,i){return self.line(d)+"z";})
-           // .attr("stroke", d => self.config.cscale( self.cvalue(d) ))
-            .attr("stroke", function(d,i){return i ? "red": "blue";})
+            .attr("d", function (d, i) { return self.line(d) + "z"; })
+            // .attr("stroke", d => self.config.cscale( self.cvalue(d) ))
+            .attr("stroke", function (d, i) { return i ? "red" : "blue"; })
             .attr("stroke-width", 2);
         self.chart.selectAll("path.grid")
             .data(self.grid)
             .enter()
             .append("path")
-            .attr("d", function(d,i){return self.line(d)+"z";})
+            .attr("d", function (d, i) { return self.line(d) + "z"; })
             .attr("stroke", "black")
             .attr("stroke-dasharray", "2")
             .attr("fill", "none");
@@ -69,13 +69,13 @@ class RadarChart {
             .data(self.label)
             .enter()
             .append("text")
-            .text(function(d, i){ return i+1; })
+            .text(function (d, i) { return i + 1; })
             .attr("text-anchor", "middle")
             .attr("dominant-baseline", "middle")
-            .attr('x', function(d, i){ console.log(self.rScale(d)); return self.rScale(d) * Math.cos(2 * Math.PI / 3*i - (Math.PI / 2)) + self.config.width/2; })
-            .attr('y', function(d, i){ return self.rScale(d) * Math.sin(2 * Math.PI / 3*i - (Math.PI / 2)) + self.config.width/2; })
+            .attr('x', function (d, i) { console.log(self.rScale(d)); return self.rScale(d) * Math.cos(2 * Math.PI / 3 * i - (Math.PI / 2)) + self.config.width / 2; })
+            .attr('y', function (d, i) { return self.rScale(d) * Math.sin(2 * Math.PI / 3 * i - (Math.PI / 2)) + self.config.width / 2; })
             .attr("font-size", "15px");
-    
+
         self.svg.selectAll("path")
             .attr('fill', 'none');
     }
