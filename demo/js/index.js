b21479e52d6eb711265effdcf5999bd96a6c50d7
horioka <1735076@stu.kobe-u.ac.jp>
2022-06-12 17:04:37 +0900

    fix

--- demo/js/index.js
+++ demo/js/index.js
@@ -17,7 +17,7 @@ var svg = $body
     });
 
 // 投影法の指定
-var projectionOption = g3.geoMercator()
+var projectionOption = d3.geoMercator()
 	.center([137, 35])				// 中心の座標を指定
 	.scale(1800)					// スケール（ズーム）の指定
 	.translate([width  / 2, height  / 2]); // 移動する
