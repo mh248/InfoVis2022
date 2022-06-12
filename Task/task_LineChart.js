75a204a0cb4d8bdeba208b740cce67006a5aa1cc
horioka <1735076@stu.kobe-u.ac.jp>
2022-06-12 21:46:25 +0900

    fix

--- Task/task_LineChart.js
+++ Task/task_LineChart.js
@@ -86,8 +86,8 @@ class LineChart {
             .domain([0, 10])
             .range([0, self.config.width/2 - self.config.margin.top])
         self.line = d3.line()
-            .x(function(d,i){return rScale(d) * Math.cos(2 * Math.PI / 3 * i - (Math.PI / 2)) + self.config.width/2;})
-            .y(function(d,i){return rScale(d) * Math.sin(2 * Math.PI / 3 * i - (Math.PI / 2)) + self.config.width/2;})
+            .x(function(d,i){return self.rScale(d) * Math.cos(2 * Math.PI / 3 * i - (Math.PI / 2)) + self.config.width/2;})
+            .y(function(d,i){return self.rScale(d) * Math.sin(2 * Math.PI / 3 * i - (Math.PI / 2)) + self.config.width/2;})
         
         self.render();
     }
